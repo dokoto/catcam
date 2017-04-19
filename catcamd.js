cc8613af5af7c97e6aefc208b2f4592f87fd751e
Alfaro Sierra <manuel.alfaro-sierra-external@capgemini.com>
2017-04-19 17:03:47 +0200

    Developing...

--- catcamd.js
+++ catcamd.js
@@ -2,21 +2,26 @@
 
 // ffmpeg -rtbufsize 1500M -f dshow -r 30 -i video="Integrated Camera" -f h264 -vcodec libx264  udp://127.0.0.1:8001
 // ffmpeg -rtbufsize 1500M -f dshow -r 30 -i video="Integrated Camera":audio="Microphone (Realtek High Defini" -crf 0 -vcodec libx264 -acodec pcm_s16le -ac 1 -ar 44100  -f flv http://127.0.0.1:8001
+// ffmpeg -rtbufsize 1500M -f dshow -framerate 25 -video_size 640x480 -i video="Integrated Camera" -vcodec libvpx -b:v 3500k -r 25 -crf 10 -quality realtime -speed 16 -threads 2 -an -g 25 -f webm http://127.0.0.1:8001
+// ffmpeg -rtbufsize 1500M -f dshow -framerate 25 -video_size 640x480 -i video="Integrated Camera"  -vcodec libx264 -profile:v main -g 25 -r 25 -b:v 500k -keyint_min 250 -strict experimental -pix_fmt yuv420p -movflags empty_moov+default_base_moof -an -preset ultrafast -f mp4 http://127.0.0.1:8001
+
+ // **** https://github.com/kmoskwiak/node-tcp-streaming-server/tree/master/client/js
 
 
 // https://ricochen.wordpress.com/2011/10/14/learning-node-js-socket-io-a-simple-streaming-example/
 // https://github.com/phoboslab/jsmpeg/blob/master/websocket-relay.js
 // http://janganstop.com/news/HTML5-Live-Video-Streaming-via-WebSockets
 // https://github.com/websockets/ws
+// http://web-engineering.info/node/57
 
 const http = require('http');
 const ws = require('socket.io')();
 const WEBSOCKET_PORT = 8002;
 const STREAM_PORT = 8001;
 
-ws.on('connection', client => {
-    console.log('New WebSocket Connection: ', client.upgradeReq.socket.remoteAddress, client.upgradeReq.headers['user-agent']);
-    client.on('close', function(code, message) {
+ws.on('connection', sockConn => {
+    console.log('New WebSocket Connection: ', sockConn.conn.remoteAddress, sockConn.client.request.headers['user-agent']);
+    sockConn.on('close', function(code, message) {
         console.log('Disconnected WebSocket');
     });
 });
@@ -28,7 +33,7 @@ const httpServer = http.createServer((req, res) => {
 
     res.connection.setTimeout(0);
     req.on('data', function(data) {
-        ws.emit('streaming', data);
+        ws.emit('onChunk', data);
     });
 
     req.on('end', function() {
