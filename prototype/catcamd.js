6fc12518aa19b7fc3d177e7e7d8cd1499f270832
administrador <dokoto.moloko@gmail.com>
2017-05-05 22:16:01 +0200

    Dev

--- prototype/catcamd.js
+++ prototype/catcamd.js
@@ -13,6 +13,7 @@
  */
 
 const https = require('https');
+const http = require('http');
 const express = require('express');
 const passport = require('passport');
 const session = require('express-session');
@@ -20,6 +21,8 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const mongoDBStore = require('connect-mongodb-session')(session);
+const crypto = require('crypto');
+const format = require('biguint-format');
 const fs = require('fs');
 const ws = require('socket.io')();
 const spawn = require('child_process').spawn;
@@ -29,6 +32,7 @@ const GOOGLE_CLIENT_ID = '702802836349-cmpihi89o5pp2mh85cl8t06g2k9jmjuu.apps.goo
 const GOOGLE_CLIENT_SECRET = '7v7XQ2fl-PfvMBSwGrfdMqDP';
 const WEBSOCKET_PORT = 4445;
 const HTTPS_PORT = 4443;
+const STREAM_PORT = 8001;
 const PUBLIC_IP = 'titan.homelinux.net';
 const LOCAL_IP = Object.keys(ifs)
   .map(x => ifs[x].filter(y => y.family === 'IPv4' && !y.internal)[0])
@@ -38,7 +42,36 @@ const ffCmd = {
   FFMPEG_CMD_MP4_WIN: 'ffmpeg -rtbufsize 1500M -f dshow -framerate 25 -video_size 640x480 -i video="Integrated Camera"  -vcodec libx264 -profile:v main -g 25 -r 25 -b:v 500k -keyint_min 250 -strict experimental -pix_fmt yuv420p -movflags empty_moov+omit_tfhd_offset+frag_keyframe+default_base_moof -an -preset ultrafast -f mp4 http://127.0.0.1:8001',
   FFMPEG_CMD_WEBM_WIN: 'ffmpeg -rtbufsize 1500M -f dshow -framerate 25 -video_size 640x480 -i video="Integrated Camera" -vcodec libvpx -b:v 3500k -r 25 -crf 10 -quality realtime -speed 16 -threads 2 -an -g 25 -f webm http://127.0.0.1:8001',
   FFMPEG_CMD_MP4_LINUX: 'ffmpeg -f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0  -vcodec libx264 -profile:v main -g 25 -r 25 -b:v 500k -keyint_min 250 -strict experimental -pix_fmt yuv420p -movflags empty_moov+omit_tfhd_offset+frag_keyframe+default_base_moof -an -preset ultrafast -f mp4 http://127.0.0.1:8001',
-  FFMPEG_CMD_WEBM_LINUX: 'ffmpeg  -f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0 -vcodec libvpx -b:v 3500k -r 25 -crf 10 -quality realtime -speed 16 -threads 2 -an -g 25 -f webm http://127.0.0.1:8001',
+  FFMPEG_CMD_WEBM_LINUX: [
+    '-f',
+    'v4l2',
+    '-framerate',
+    '25',
+    '-video_size',
+    '640x480',
+    '-i',
+    '/dev/video0',
+    '-vcodec',
+    'libvpx',
+    '-b:v',
+    '3500k',
+    '-r',
+    '25',
+    '-crf',
+    '10',
+    '-quality',
+    'realtime',
+    '-speed',
+    '16',
+    '-threads',
+    '2',
+    '-an',
+    '-g',
+    '25',
+    '-f',
+    'webm',
+    `http://${ LOCAL_IP }:${ STREAM_PORT }`,
+  ],
   FFMPEG_CMD_WEBM_WIN_SPLIT: [
     '-rtbufsize',
     '1500M',
@@ -69,12 +102,23 @@ const ffCmd = {
     '25',
     '-f',
     'webm',
-    'http://127.0.0.1:8001',
+    `http://${ LOCAL_IP }:${ STREAM_PORT }`,
   ],
 };
 
-const FFMPEG_CMD = ffCmd.FFMPEG_CMD_WEBM_WIN_SPLIT;
+function generateRandHash() {
+  const x = crypto.randomBytes(8);
+  const hash = crypto.createHash('sha256');
+  hash.update(format(x, 'dec'));
+  return hash.digest('hex');
+}
+
+// ********************************************************
+// WEBSOCKET SERVER
+// *********************************************************/
+const FFMPEG_CMD = ffCmd.FFMPEG_CMD_WEBM_LINUX;
 let childProcess;
+let WS_CHANNEL;
 ws.on('connection', sockConn => {
   console.log(
     'New WebSocket Connection: ',
@@ -82,27 +126,43 @@ ws.on('connection', sockConn => {
     sockConn.client.request.headers['user-agent']
   );
 
-  if (childProcess) {
-    childProcess.kill('SIGKILL');
-    setTimeout(() => {
-      console.log('Process endded, new process..');
-      childProcess = spawn('ffmpeg', FFMPEG_CMD);
-    }, 1000);
-  } else {
-    childProcess = spawn('ffmpeg', FFMPEG_CMD);
-  }
-
   sockConn.on('close', (code, message) => {
     console.log('Disconnected WebSocket %s %s', code, message);
   });
 
+  sockConn.on('join', room => {
+    if (room === WS_CHANNEL) {
+      sockConn.join(room);
+    }
+  });
+
+  sockConn.on('start', room => {
+    if (room === WS_CHANNEL) {
+      if (childProcess) {
+        childProcess.kill('SIGKILL');
+        setTimeout(() => {
+          console.log('Process endded, new process..');
+          childProcess = spawn('ffmpeg', FFMPEG_CMD);
+        }, 1000);
+      } else {
+        console.log(' COMM: %s', FFMPEG_CMD.join(' '));
+        childProcess = spawn('ffmpeg', FFMPEG_CMD);
+      }
+    }
+  });
+
   sockConn.on('disconnect', () => {
     console.log('Client disconected');
+    sockConn.leave(WS_CHANNEL);
     ws.emit('user disconnected');
   });
 });
 
 ws.listen(WEBSOCKET_PORT);
+
+// ********************************************************
+// EXPRESS SERVER
+// *********************************************************/
 
 passport.serializeUser((user, done) => {
   done(null, user);
@@ -188,15 +248,8 @@ app.get('/logout', (req, res) => {
 
 app.get('/stream', ensureAuthenticated, (req, res) => {
   console.log(`/stream: ${ req.socket.remoteAddress }:${ req.socket.remotePort }`);
-
-  res.connection.setTimeout(0);
-  req.on('data', data => {
-    ws.emit('onChunk', data);
-  });
-
-  req.on('end', () => {
-    console.log('close');
-  });
+  WS_CHANNEL = generateRandHash();
+  res.status(200).json(JSON.stringify({ channel: WS_CHANNEL }));
 });
 
 app.get(
@@ -222,5 +275,24 @@ const key = fs.readFileSync('./certs/key.pem');
 
 https.createServer({ cert, key }, app).listen(HTTPS_PORT);
 
-console.log('PUBLIC REST API on https://%s:%s/camcat/', PUBLIC_IP, HTTPS_PORT);
+// ********************************************************
+// STREAM HTTP SERVER
+// *********************************************************/
+http
+  .createServer((req, res) => {
+    console.log(`Stream Connected: ${ req.socket.remoteAddress }:${ req.socket.remotePort }`);
+
+    res.connection.setTimeout(0);
+    req.on('data', data => {
+      ws.to(WS_CHANNEL).emit('onChunk', data);
+    });
+
+    req.on('end', () => {
+      console.log('close');
+    });
+  })
+  .listen(STREAM_PORT, LOCAL_IP);
+
+console.log('LOCAL HTTP BROADCAST-VIDEO on http://%s:%s', LOCAL_IP, STREAM_PORT);
+console.log('PUBLIC REST API on https://%s:%s', PUBLIC_IP, HTTPS_PORT);
 console.log('PUBLIC WEBSOCKET ws://%s:%d/', PUBLIC_IP, WEBSOCKET_PORT);
