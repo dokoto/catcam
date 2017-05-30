5c5974321fa903593ddb0cdeffd6f7d6220c1b79
Alfaro Sierra <manuel.alfaro-sierra-external@capgemini.com>
2017-05-30 16:28:53 +0200

    Dev

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,19 +1,31 @@
 module.exports = function (options) {
   return {
-    'webpack': {
-      model: {
-        name: 'Webpack: packing...',
-      },
+    webpack: {
+      description: 'Webpack: packing...',
       tasks: [
         `node ${ options.webpack.bin } --config ${ options.webpack.config } --progress --profile --colors --env.target='${ options.params.target }' --env.env='${ options.params.env }' --env.platform='${ options.params.platform }' --env.version='${ options.params.version }' --env.languaje='${ options.params.languaje }'`,
       ],
     },
+    'copy-sources': {
+      description: 'Coping sources...',
+      tasks: [
+        {
+          define: {
+            fs: 'fs-extra',
+          },
+          func: fs => {
+            fs.removeSync(options['copy-sources'].dest);
+            fs.copySync(options.['copy-sources'].source, options.['copy-sources'].dest);
+          },
+        },
+      ],
+    },
+    'cordova-build': {
+      description: 'Apache Cordova building....',
+    },
     'build-native-loc': {
-      model: {
-        name: 'Building for web',
-        extends: ['webpack'],
-      },
-      tasks: [],
+      description: 'Building for native...',
+      tasks: ['@webpack', '@cordova-build', '@copy-sources'],
     },
   };
 };
