3143a7a1098bd524be9c5fbe22383c4f578cd7a1
Alfaro Sierra <manuel.alfaro-sierra-external@capgemini.com>
2017-06-01 16:01:03 +0200

    dev

--- scripts/builder.js
+++ scripts/builder.js
@@ -2,14 +2,17 @@ const Params = require('./params');
 const Tasks = require('./tasks');
 const Executer = require('./executer');
 
-
 const parsedParams = Params.get();
+console.log(JSON.stringify(parsedParams));
 if (parsedParams.options.help) {
   console.log('Usage:');
   console.log(parsedParams.usage);
   process.exit(-1);
 }
 
-
-const executer = new Executer(Tasks(parsedParams.options));
+const executer = new Executer(
+  Tasks({
+    params: parsedParams.options,
+  })
+);
 executer.run(`build-${ parsedParams.options.platform }-${ parsedParams.options.target }`);
